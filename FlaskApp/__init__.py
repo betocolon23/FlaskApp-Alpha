90f29d6ec6e9d5abfad98b3ceb648e8db4993591
Beto Colon <betocolon23@gmail.com>
2018-08-16 18:45:20 -0400

    test server

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,9 +1,47 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
+from forms import ContactForm
+from flask_mail import Mail, Message
+
+
+mail = Mail()
+
 app = Flask(__name__)
 
+app.secret_key = 'albertocolonnegron1995'
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'betocolon23@gmail.com'
+app.config["MAIL_PASSWORD"] = 'Tobe2013'
+ 
+mail.init_app(app)
+
 @app.route('/')
-def homepage():
-   return render_template('index.html')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+  form = ContactForm()
+ 
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields are required.')
+      return render_template('contact.html', form=form)
+    else:
+      msg = Message(form.subject.data, sender='betocolon23@gmail.com', recipients=['betocolon23@gmail.com'])
+      msg.body = """
+      From: %s <%s>
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+ 
+      return render_template('contact.html', success=True)
+ 
+  elif request.method == 'GET':
+    return render_template('contact.html', form=form)
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
---
+++ FlaskApp/forms.py
@@ -0,0 +1,10 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, TextField, SubmitField, validators, TextAreaField
+from wtforms.validators import DataRequired, Email
+
+class ContactForm(Form):
+  name = StringField("Name",  [DataRequired("Please enter your name.")])
+  email = StringField("Email",  [DataRequired("Please enter your email address."), Email("This field requires a valid email address")])
+  subject = StringField("Subject",  [DataRequired("Please enter a subject.")])
+  message = TextAreaField("Message",  [DataRequired("Please enter a message")])
+  submit = SubmitField("Send")
